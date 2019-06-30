2633e30169f23f1b43f5566edccda247f64e4626
zakhv <zakhv@10.147.5.135>
2019-06-30 18:19:09 -0400

    start testing on part B for project I

---
+++ Projects/src/project1/ExercisesTest.java
@@ -0,0 +1,14 @@
+package project1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExercisesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
